0f1fdd4c46f339225b6c1c60f4938d49fe9a78ec
ishaarora01 <ishaarora.87@gmail.com>
2020-10-01 18:14:44 -0400

    code

---
+++ controllers/users.js
@@ -0,0 +1,49 @@
+const User = require('../models').User;
+const Fruit = require('../models').Fruit;
+
+const renderProfile = (req, res) => {
+    console.log('User Object')
+    console.log(req.user);
+
+    User.findByPk(req.user.id, {
+        include: [
+            {
+                model: Fruit,
+                attributes: ['id', 'name']
+            }
+        ]
+    })
+    .then(userProfile => {
+        console.log(userProfile);
+        res.render('users/profile.ejs', {
+            user: userProfile
+        })
+    })
+    
+}
+
+const editProfile = (req, res) => {
+    User.update(req.body, {
+        where: {id: req.user.id},
+        returning: true
+    })
+    .then(updatedUser => {
+        res.redirect(`/users/profile`);
+    })
+    
+}
+
+const deleteUser = (req, res) => {
+    User.destroy({
+        where: {id: req.params.index}
+    })
+    .then(() => {
+        res.redirect('/users');
+    })
+}
+
+module.exports = {
+    renderProfile,
+    editProfile,
+    deleteUser
+}
